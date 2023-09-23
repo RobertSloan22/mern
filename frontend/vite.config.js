6dbe6a72da3c865234fe714224ff229bae8821ef
Robert Sloan <89170263+RobertSloan22@users.noreply.github.com>
2023-09-23 13:26:13 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'build',
+  plugins: [react()],
+  server: {
+    port: 3000,
+    // proxy is used to proxy the request to the backend
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+    },
   },
-  plugins: [react(), svgr()],
-})
+});
