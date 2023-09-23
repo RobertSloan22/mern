b0de4bd45b6dc45d47c34f2f4bd37e800f6386a4
Robert Sloan <89170263+RobertSloan22@users.noreply.github.com>
2023-09-23 15:17:58 -0500

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,33 +1,37 @@
-import jwt from 'jsonwebtoken'
-import asyncHandler from 'express-async-handler'
-import User from '../models/userModel.js'
+const jwt = require("jsonwebtoken");
 
-const protect = asyncHandler(async (req, res, next) => {
-  let token
-  const authHeader = req.headers.authorization
+const Admin = require("../../models/adminModel");
 
-  if (authHeader && authHeader.startsWith('Bearer')) {
+const authAdminProtect = async (req, res, next) => {
+  let token;
+  let admin;
+  let decoded;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.trim().startsWith("Bearer")
+  ) {
     try {
-      // extract token from authHeader string
-      token = authHeader.split(' ')[1]
-
-      // verified token returns user id
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-      // find user's obj in db and assign to req.user
-      req.user = await User.findById(decoded.id).select('-password')
-
-      next()
+      //Get Token from header
+      token = req.headers.authorization.split(" ")[1];
+      //verify token
+      decoded = jwt.verify(token, process.env.JWT_SECRET);
+      //Get Admin from Token
+      admin = await Admin.findById(decoded.id);
+      if (!admin) {
+        return res.status(401).send("Not Authorized with invalid token");
+      }
+      //pass admin object to next middleware
+      req.admin = await Admin.findById(decoded.id);
+      next();
     } catch (error) {
-      res.status(401)
-      throw new Error('Not authorized, invalid token')
+      if (!decoded || !(await Admin.findById(decoded.id)))
+        return res.status(401).send("Not Authorized with invalid token");
+      return res.status(500).send("Ooops!! Something Went Wrong, Try again...");
     }
   }
+  if (!token) return res.status(401).send("Not Authorized without token");
+};
 
-  if (!token) {
-    res.status(401)
-    throw new Error('Not authorized, no token found')
-  }
-})
-
-export { protect }
+module.exports = {
+  authAdminProtect,
+};
