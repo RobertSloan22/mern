052cc73fb61ac81d839da7eadc5e872e01a5856e
Robert Sloan <89170263+RobertSloan22@users.noreply.github.com>
2023-09-23 20:15:56 -0500

    Create cors.js

---
+++ backend/config/cors.js
@@ -0,0 +1,42 @@
+import Cors from 'cors';
+
+// Devlopement CORS Configurations//
+
+const devWhitelist = ["http://localhost:8080"];
+
+const corsDevOptions = {
+  origin: function (origin, callback) {
+    if (!origin || devWhitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+
+  credentials: true,
+};
+
+// Production CORS Configurations//
+
+const domainsFromEnv = process.env.CORS_DOMAINS || "";
+
+const productionWhitelist = domainsFromEnv
+  .split(",")
+  .map((item) => item.trim());
+
+const corsProOptions = {
+  origin: function (origin, callback) {
+    if (!origin || productionWhitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+
+  credentials: true,
+};
+
+module.exports = {
+  corsDevOptions,
+  corsProOptions,
+};
